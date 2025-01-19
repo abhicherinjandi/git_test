52222f4167be700e84ed181b6e0c070df904ca6a
Abhilash CV <abhilash,cv@gmail.com>
2025-01-19 18:02:16 +0530

    New Feature Function Added

--- src/Feature1.java
+++ src/Feature1.java
@@ -3,4 +3,8 @@ public class Feature1 {
     public static void main(String[] args) {
 
     }
+
+    public void newFeatureFunction(){
+        System.out.println("New Feature Function Added");
+    }
 }
